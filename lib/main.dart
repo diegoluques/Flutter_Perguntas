92256a3285ab1523bee605a04ca17b767f624271
Diego <luquesdiego@outlook.com>
2020-07-05 16:46:00 -0400

    Final projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_perguntas/questao.dart';
+import 'package:flutter_perguntas/questionario.dart';
+import 'package:flutter_perguntas/resultado.dart';
 
 void main() {
   runApp(PerguntaApp());
@@ -11,49 +12,78 @@ class PerguntaApp extends StatefulWidget {
 }
 
 class _PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
-  void _responder() {
+  var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
+  final _perguntas = const [
+    {
+      "texto": "Qual é a sua cor favorita?",
+      "respostas": [
+        {"texto": "Preto", "pontuacao": 10},
+        {"texto": "Vermelho", "pontuacao": 5},
+        {"texto": "Branco", "pontuacao": 3},
+        {"texto": "Verde", "pontuacao": 1},
+      ],
+    },
+    {
+      "texto": "Qual é o seu animal favorito?",
+      "respostas": [
+        {"texto": "Coelho", "pontuacao": 6},
+        {"texto": "Cachorro", "pontuacao": 3},
+        {"texto": "Gato", "pontuacao": 2},
+        {"texto": "Leão", "pontuacao": 8},
+      ],
+    },
+    {
+      "texto": "Qual é o seu instrutor favorito?",
+      "respostas": [
+        {"texto": "Maria", "pontuacao": 9},
+        {"texto": "João", "pontuacao": 7},
+        {"texto": "Leo", "pontuacao": 10},
+        {"texto": "Pedro", "pontuacao": 4},
+      ],
+    },
+  ];
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+
+      print(_pontuacaoTotal);
+    }
+  }
+
+  void _reiniciarQuestionario() {
     setState(() {
-      perguntaSelecionada++;
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
     });
-    print(perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    final pergunstas = [
-      "Qual é sua cor favorita?",
-      "Qual é seu animal favorito?",
-    ];
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter Perguntas"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Questao(pergunstas[perguntaSelecionada]),
-              RaisedButton(
-                child: Text("Resposta 1"),
-                onPressed: _responder,
-              ),
-              RaisedButton(
-                child: Text("Resposta 2"),
-                onPressed: _responder,
-              ),
-              RaisedButton(
-                child: Text("Resposta 3"),
-                onPressed: _responder,
-              ),
-            ],
-          ),
-        ),
+        body: temPerguntaSelecionada
+            ? Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Qustionario(
+                  perguntas: _perguntas,
+                  perguntaSelecionada: _perguntaSelecionada,
+                  quandoResponder: _responder,
+                ),
+              )
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
